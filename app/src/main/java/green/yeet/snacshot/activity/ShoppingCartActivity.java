9a4bdbf7d3c1334ea3e9ba0b27536fdb1edac089
rmcreyes <robinreyes1998@yahoo.com>
2019-02-10 09:32:57 -0800

    finished design of shopping cart

--- app/src/main/java/green/yeet/snacshot/activity/ShoppingCartActivity.java
+++ app/src/main/java/green/yeet/snacshot/activity/ShoppingCartActivity.java
@@ -1,8 +1,19 @@
 package green.yeet.snacshot.activity;
 
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,6 +26,9 @@ import green.yeet.snacshot.model.GroceryItem;
 public class ShoppingCartActivity extends AppCompatActivity {
 
     private ListView itemList;
+    private FloatingActionButton addButton;
+    private ImageView checkoutButton;
+    private ActionBarDrawerToggle toggle;
 
     private List<GroceryItem> groceryItems;
 
@@ -22,11 +36,40 @@ public class ShoppingCartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
-        setTitle("Shopping Cart");
-
-        itemList = findViewById(R.id.item_list);
 
         fillItems();
+
+        itemList = findViewById(R.id.item_list);
+        addButton = findViewById(R.id.add_btn);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ShoppingCartActivity.this, "add food", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkoutButton = findViewById(R.id.checkout_btn);
+        View nutritionBottomSheet = findViewById(R.id.nutrients_bottom_sheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(nutritionBottomSheet);
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                checkoutButton.animate().scaleX(1 - slideOffset).scaleY(1 - slideOffset).setDuration(0).start();
+            }
+        });
+
+        checkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ShoppingCartActivity.this, "checkout", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         CartItemAdapter cartItemAdapter = new CartItemAdapter(groceryItems, getApplicationContext());
         itemList.setAdapter(cartItemAdapter);
