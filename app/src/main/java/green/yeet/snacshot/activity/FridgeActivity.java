b1782c3e0bdeaf67872d855d7f2517bbcea63b2b
orayasrimokla <oraya.srimokla@gmail.com>
2019-02-10 11:33:09 -0800

    linked fridge

--- app/src/main/java/green/yeet/snacshot/activity/FridgeActivity.java
+++ app/src/main/java/green/yeet/snacshot/activity/FridgeActivity.java
@@ -2,6 +2,9 @@ package green.yeet.snacshot.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -25,13 +28,27 @@ import green.yeet.snacshot.model.GroceryItem;
             setContentView(R.layout.activity_fridge);
             setTitle("Fridge");
 
+
             itemList = findViewById(R.id.item_list);
 
             fillItems();
 
-            FridgeItemAdapter fridgeItemAdapter = new FridgeItemAdapter(groceryItems, getApplicationContext());
+            itemList = findViewById(R.id.item_list);
+            FridgeItemAdapter fridgeItemAdapter = new FridgeItemAdapter(groceryItems, this);
+            itemList.setAdapter(fridgeItemAdapter);
+
+            itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    GroceryItem tappedGroceryItem = groceryItems.get(position);
+
+                }
+            });
+
             itemList.setAdapter(fridgeItemAdapter);
         }
+
+
 
         private void fillItems() {
             groceryItems = new ArrayList<GroceryItem>();
