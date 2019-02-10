6f67085a30f1aeec988a26eeec2f06ceb70a52d5
rmcreyes <robinreyes1998@yahoo.com>
2019-02-09 21:55:35 -0800

    first commit

---
+++ app/src/main/java/green/yeet/snacshot/activity/ItemValidationActivity.java
@@ -0,0 +1,15 @@
+package green.yeet.snacshot.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import green.yeet.snacshot.R;
+
+public class ItemValidationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_validation);
+    }
+}
